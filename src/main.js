65c3a1bf14c3502cf726af6142cd225a2f64396e
Sarah Schwartz <seschwartz8@gmail.com>
2020-02-04 09:20:56 -0800

    change tests folder

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import $ from 'jquery';
+// import $ from 'jquery';
 import { thingFunction } from './thing.js'
 
 
