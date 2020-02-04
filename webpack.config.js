3a5a4b71e2cab1fd0af6c6a87e6baad6480fcc52
Student <example@example.com>
2020-02-04 11:10:53 -0800

    Add File Loader and URL Loader to package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "eslint-loader"
+      },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader?limit=100000' 
       }
     ]
   }
